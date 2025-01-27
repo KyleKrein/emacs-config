173299ea8b5c74be4ce43ff807dc695db1e7815a
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-27 19:56:46 +0100

    First nix-emacs config

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "KyleKrein's emacs flake configuration";
+
+  nixConfig = {
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    emacs-overlay,
+  }: {
+    packages.x86_64-linux.default = import ./package.nix {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [emacs-overlay.overlays.default];
+      };
+    };
+    packages.aarch64-linux.default = import ./package.nix {
+      pkgs = import nixpkgs {
+        system = "aarch64-linux";
+        overlays = [emacs-overlay.overlays.default];
+      };
+    };
+    packages.x86_64-linux.x11 = import ./package.nix {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [emacs-overlay.overlays.default];
+      };
+      x11 = true;
+    };
+    packages.aarch64-linux.x11 = import ./package.nix {
+      pkgs = import nixpkgs {
+        system = "aarch64-linux";
+        overlays = [emacs-overlay.overlays.default];
+      };
+      x11 = true;
+    };
+  };
+}
---
+++ init.el
@@ -0,0 +1,5 @@
+;;; -*- lexical-binding: t; -*-
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
