7a2324a826ae339d5d416a178d72217d8e4a5dce
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-30 22:38:19 +0100

    Added some code for android gui version

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; -*- lexical-binding: t; -*-
+(when (string-equal system-type "android")
+  ;; Add Termux binaries to PATH environment
+  (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
+    (setq exec-path (append exec-path (list termuxpath)))))
+
