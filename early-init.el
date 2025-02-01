720c1d126adc82d3ba51fdaef534f016fe4f5a5c
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-02-01 15:39:49 +0100

    Android update

--- early-init.el
+++ early-init.el
@@ -3,5 +3,9 @@
   ;; Add Termux binaries to PATH environment
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
     (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
-    (setq exec-path (append exec-path (list termuxpath)))))
+    (setq exec-path (append exec-path (list termuxpath))))
+  (let ((termuxlib "/data/data/com.termux/files/usr/lib"))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxlib))
+    (setq exec-path (append exec-path (list termuxlib)))
+)
 
