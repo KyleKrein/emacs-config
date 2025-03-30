bc2d813b943a28d1426cdce6dcf9e34f9cf29fc9
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-03-30 17:24:21 +0200

    ??

--- init.el
+++ init.el
@@ -7,12 +7,12 @@
 ;; (add-to-list 'package-archives
 ;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(use-package edraw-org
-  :ensure t
-  ;;:after org
-  :vc (:url "https://github.com/misohena/el-easydraw"
-            :branch master))
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(use-package edraw-org
+  :ensure t
+  ;;:after org
+  :vc (:url "https://github.com/misohena/el-easydraw.git"
+            :branch master))
